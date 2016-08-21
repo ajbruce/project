d71bdd44deda724ae08bd64a8ab8c00cc8f20065
ajbruce <ariel.bruce@mail.huji.ac.il>
2016-08-21 12:35:14 +0300

    Update lightfield.h

--- lightfield.h
+++ lightfield.h
@@ -1,11 +1,36 @@
-class Pose {
-    int uCoord;
-    int vCoord;
+#include <stdlib>
+#include <vector>
+#include <string
 
-};
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <iostream>
+
+typedef struct Coordinate
+{
+    int x;
+    int y;
+
+public:
+    Coordinate (int x, int y) : x(x), y(y) {}
+    Coordinate difference (Coordinate & other)
+    {
+        return Coordinate(this->x - other.x, this->y - other.y);
+    }
+} Coordinate;
+
 
 class RayBundle {
-    
+    Coordinate pose;
+    Coordinate point;
+    int sizeVertical;
+    int sizeHorizontal;
+
+    RayBundle(Coordinate pose, Coordinate point, int sizeVertical,
+              int sizeHorizontal) : pose(pose), point(point),
+                                    sizeVertical(sizeVertical),
+                                    sizeHorizontal(sizeHorizontal) {}
+
 
 };
 
