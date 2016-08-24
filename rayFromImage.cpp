e5e4a712e2623564862de640937446ccead6b4ec
ajbruce <ariel.bruce@mail.huji.ac.il>
2016-08-24 11:23:35 +0300

    Update rayFromImage.cpp

--- rayFromImage.cpp
+++ rayFromImage.cpp
@@ -1,1 +1,40 @@
+#include <stdlib>
+#include <vector>
+#include <string>
+#include "lightfield.h"
+#include "calculateHomography.cpp"
+#include "poseFromHomography.cpp"
 
+using namespace std;
+
+
+//Create array of images from input
+
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <iostream>
+
+using namespace cv;
+using namespace std;
+
+/**
+ *Function that adds the data from the image to the
+ *lightfield.  Uses homography helper functions to find the 
+ *pose of the camera.  Then puts that information into the 
+ *lightfield. If there is already data there, then it takes the
+ *average
+ */
+int rayFromImage(Lightfield * currField, Mat * image) {
+
+	Mat H = calculateHomography(currField->originImage, image)
+	
+	// 3x4 matrix, the camera pose
+	Mat pose;
+	
+	poseFromHomography(H, pose);
+
+
+
+
+
+}
