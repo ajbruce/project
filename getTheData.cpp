e7f0b812eaa46f194eed6390fbb6292e80493b27
ajbruce <ariel.bruce@mail.huji.ac.il>
2016-10-06 07:54:14 +0300

    Update getTheData.cpp

--- getTheData.cpp
+++ getTheData.cpp
@@ -11,9 +11,6 @@
 
 using namespace cv;
 
-#define FAILURE -1
-#define SUCCESS 0
-
 /**
  *Function that adds the data from the image to the
  *lightfield.  Uses homography helper functions to find the 
@@ -31,8 +28,11 @@ int getTheData(Lightfield * currField) {
 		Mat H;
 		int res = FAILURE;
 		int i;
-		for(i = 0; i < NUMFRAMINGIMAGES; ++i) {
-			res = calculateHomography(it, currField->frameImages.at(i), H);
+
+		Mat image = *it;
+
+		for(i = 0; i < NUM_FRAMING_IMAGES; ++i) {
+			res = calculateHomography(image, currField->frameImages.at(i), H);
 			if(res == SUCCESS) {
 				break;
 			}
@@ -50,19 +50,9 @@ int getTheData(Lightfield * currField) {
 			return FAILURE;
 		}
 		
-		//find coordinates from homography and pose
-		
-		//insert the data into the lightfield structure
-		
-		//find next pose
-		
+		//call rayFromImage function	
 		
 		
 	}
 	
-
-
-
-
-
 }
